2ea7f9771377ab97c49b5ff0eb933c5415bcd504
Chris Campbell <chris@ctcampbell.com>
2019-06-05 09:18:26 +0100

    Add User-Agent header

--- example.py
+++ example.py
@@ -4,11 +4,13 @@ from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC
 
 
 api_base = "https://api.veracode.com/appsec/v1"
+headers = {"User-Agent": "Python HMAC Example"}
+
 
 if __name__ == "__main__":
 
     try:
-        response = requests.get(api_base + "/applications", auth=RequestsAuthPluginVeracodeHMAC())
+        response = requests.get(api_base + "/applications", auth=RequestsAuthPluginVeracodeHMAC(), headers=headers)
     except requests.RequestException as e:
         print("Whoops!")
         print(e)
